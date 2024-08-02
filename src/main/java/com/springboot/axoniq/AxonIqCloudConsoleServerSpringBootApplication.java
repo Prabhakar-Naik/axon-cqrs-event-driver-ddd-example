144c3330370ef7917cf1756ef2550953aecea303
Prabhakar Naik Kimavath <prabhakarnaik225@gmail.com>
2024-08-02 18:52:51 +0530

    project setup

---
+++ src/main/java/com/springboot/axoniq/AxonIqCloudConsoleServerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.axoniq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonIqCloudConsoleServerSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxonIqCloudConsoleServerSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/axoniq/AxonIqCloudConsoleServerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.axoniq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxonIqCloudConsoleServerSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
